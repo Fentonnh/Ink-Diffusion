6b36bf9d6e178be594a0f860189376c3c65529f4
Fentonnh <fentonnh@gmail.com>
2025-07-03 17:59:52 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
-let shaderProgram;
+let theShader;
 let gridSlider, bleedSlider, invertCheckbox;
 
 function preload() {
-  shaderProgram = loadShader('shader.vert', 'shader.glsl');
+  theShader = loadShader('shader.vert', 'shader.glsl');
 }
 
 function setup() {
-  let cnv = createCanvas(windowWidth - 250, windowHeight, WEBGL);
-  cnv.parent('canvas-container');
+  const canvas = createCanvas(windowWidth - 250, windowHeight, WEBGL);
+  canvas.parent('canvas-container');
   noStroke();
 
   gridSlider = select('#grid');
@@ -16,13 +16,13 @@ function setup() {
 }
 
 function draw() {
-  shader(shaderProgram);
+  shader(theShader);
 
-  shaderProgram.setUniform('u_resolution', [width, height]);
-  shaderProgram.setUniform('u_time', millis() / 1000.0);
-  shaderProgram.setUniform('u_gridScale', gridSlider.value());
-  shaderProgram.setUniform('u_bleed', bleedSlider.value());
-  shaderProgram.setUniform('u_invert', invertCheckbox.checked() ? 1.0 : 0.0);
+  theShader.setUniform('u_resolution', [width, height]);
+  theShader.setUniform('u_time', millis() / 1000.0);
+  theShader.setUniform('u_gridScale', float(gridSlider.value()));
+  theShader.setUniform('u_bleed', float(bleedSlider.value()));
+  theShader.setUniform('u_invert', invertCheckbox.checked() ? 1.0 : 0.0);
 
   rect(-width / 2, -height / 2, width, height);
 }
