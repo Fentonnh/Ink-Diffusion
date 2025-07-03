6db35b862a95e3aa42c50bf756471d5a5af16eaa
Fentonnh <fentonnh@gmail.com>
2025-07-03 18:04:11 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,32 +1,36 @@
-let theShader;
+let shaderProgram;
 let gridSlider, bleedSlider, invertCheckbox;
 
 function preload() {
-  theShader = loadShader('shader.vert', 'shader.glsl');
+  shaderProgram = loadShader('shader.vert', 'shader.glsl');
 }
 
 function setup() {
-  const canvas = createCanvas(windowWidth - 250, windowHeight, WEBGL);
+  const container = select('#canvas-container');
+  let canvas = createCanvas(container.width, container.height, WEBGL);
   canvas.parent('canvas-container');
+
   noStroke();
+  pixelDensity(1);
 
   gridSlider = select('#grid');
   bleedSlider = select('#bleed');
   invertCheckbox = select('#invert');
 }
 
-function draw() {
-  shader(theShader);
-
-  theShader.setUniform('u_resolution', [width, height]);
-  theShader.setUniform('u_time', millis() / 1000.0);
-  theShader.setUniform('u_gridScale', float(gridSlider.value()));
-  theShader.setUniform('u_bleed', float(bleedSlider.value()));
-  theShader.setUniform('u_invert', invertCheckbox.checked() ? 1.0 : 0.0);
-
-  rect(-width / 2, -height / 2, width, height);
+function windowResized() {
+  const container = select('#canvas-container');
+  resizeCanvas(container.width, container.height);
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth - 250, windowHeight);
+function draw() {
+  shader(shaderProgram);
+
+  shaderProgram.setUniform('u_resolution', [width, height]);
+  shaderProgram.setUniform('u_time', millis() / 1000.0);
+  shaderProgram.setUniform('u_gridScale', float(gridSlider.value()));
+  shaderProgram.setUniform('u_bleed', float(bleedSlider.value()));
+  shaderProgram.setUniform('u_invert', invertCheckbox.checked() ? 1.0 : 0.0);
+
+  rect(0, 0, width, height);
 }
