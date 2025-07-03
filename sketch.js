455763f669183579869e351e4b1cd25499c64996
Fentonnh <fentonnh@gmail.com>
2025-07-03 17:13:37 -0400

    test sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,22 @@
-let inkShader;
+let theShader;
 
 function preload() {
-  inkShader = loadShader('shader.vert', 'shader.frag');
+  theShader = loadShader("shader.vert", "shader.frag",
+    () => console.log("Shader loaded"),
+    (err) => console.error("Shader failed:", err)
+  );
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   noStroke();
-  shader(inkShader);
 }
 
 function draw() {
-  inkShader.setUniform('u_time', millis() / 1000.0);
-  inkShader.setUniform('u_resolution', [width, height]);
+  shader(theShader);
+  theShader.setUniform("u_time", millis() / 1000.0);
+  theShader.setUniform("u_resolution", [width, height]);
+
+  // DRAW FULL SCREEN RECTANGLE to trigger the shader
   rect(-width / 2, -height / 2, width, height);
 }
